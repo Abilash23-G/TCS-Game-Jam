2667799aa53868eddd8abb28f7e36c2ac35307e9
Abilash23-G <80388066+Abilash23-G@users.noreply.github.com>
2021-03-22 20:04:44 +0530

    Add files via upload

---
+++ ObjectHit3.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ObjectHit3 : MonoBehaviour
+{
+    [SerializeField] GameObject finalCompound;
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] GameObject[] compounds;
+
+    bool isSpawned = false;
+
+    [SerializeField] bool[] Check;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        CheckCompound();
+
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+
+        for (int index = 0; index < compounds.Length; index++)
+        {
+            if (collision.gameObject == compounds[index])
+            {
+                Check[index] = true;
+                Destroy(compounds[index]);
+            }
+        }
+    }
+
+
+    void CheckCompound()
+    {
+        if (!isSpawned && Check[0] && Check[1] && Check[2] && Check[3] && Check[4] && Check[5])
+        {
+            Instantiate(finalCompound, spawnPoint.position, spawnPoint.rotation);
+            isSpawned = true;
+            Invoke("LoadNextLevel", 1f);
+        }
+    }
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+        isSpawned = false;
+    }
+}
