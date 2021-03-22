7eff512d11a8580eb79bf025e7cf0dd8791a41ad
Abilash23-G <80388066+Abilash23-G@users.noreply.github.com>
2021-03-22 20:30:53 +0530

    Add files via upload

---
+++ Assets/Script/LVL1/PlayerMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMover();
+    }
+
+    void PlayerMover()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(x, 0, z);
+    }
+}
